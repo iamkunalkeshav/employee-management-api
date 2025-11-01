191ddc6f21d5aecaf69d34870f32112dd56b50f6
iamkunalkeshav <kunalkeshav2002@gmail.com>
2025-11-01 21:50:27 +0530

    Initial commit - Spring Boot Employee Management API with Docker setup

---
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -0,0 +1,39 @@
+package com.example.employee.service;
+
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElseThrow();
+        employee.setName(employeeDetails.getName());
+        employee.setDepartment(employeeDetails.getDepartment());
+        employee.setEmail(employeeDetails.getEmail());
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
